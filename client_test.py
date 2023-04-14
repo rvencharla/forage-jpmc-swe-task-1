ee6fd2a2fed1641683dd6ca7d5360bf637318f2d
rvencharla <ramaraovencharla@gmail.com>
2023-04-14 14:58:21 -0500

    Assertions Tests And one more unit test Fixed client3

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    assert getDataPoint(quotes[0]) == ('ABC', 120.48, 121.2, 120.84)
+    assert getDataPoint(quotes[1]) == ('DEF', 117.87, 121.68, 119.775)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +17,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
-
+    #Assertion that getDataPoint returns a tuple
+    assert type(getDataPoint(quotes[0])) == tuple
+    
   """ ------------ Add more unit tests ------------ """
-
+  def test_getDataPoint_calculatePriceAskGreaterThanBid(self):
+     quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+     ]
 
 
 if __name__ == '__main__':
